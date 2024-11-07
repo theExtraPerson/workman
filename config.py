ad80b458747e35bef62c12cc0b5118009c867df5
theExtraPerson <akaihura22@gmail.com>
2024-11-07 09:14:19 -0800

    Updating webhook_url and requirements.txt

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Configuration fle 
 TELEGRAM_BOT_TOKEN = "7509769147:AAFPSgrR96dWVsHSQjFMKYzb0unOt1GcHvk"
 DATABASE_URL = "sqlite:///services.db"
-WEBHOOK_URL = ""
+WEBHOOK_URL = "https://workman-three.vercel.app/api/webhook"
